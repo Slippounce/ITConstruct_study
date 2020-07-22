8a2160252440a840d96d16312694123c6da96968
Slippounce <sir.shmidt-crdoc@yandex.ru>
2020-07-22 15:57:28 +0700

    use jquery

--- Блок2/template/assets/js/script.js
+++ Блок2/template/assets/js/script.js
@@ -4,16 +4,17 @@ window.onload = () => {
         let value;
         if(e.target.closest('button')) {
             if (e.target.closest('button').getAttribute('data-type') === 'minus') {
-                value = Number.parseInt(e.target.closest('button').nextElementSibling.firstChild.value);
+                value = Number.parseInt( $(e.target.closest('button')).siblings('label').children('input').attr('value'));
                 if (value > 1) {
-                    e.target.closest('button').nextElementSibling.firstChild.value = value - 1;
+                    $(e.target.closest('button')).siblings('label').children('input').attr('value', value - 1);
                 }
             }
             if (e.target.closest('button').getAttribute('data-type') === 'plus') {
-                value = Number.parseInt(e.target.closest('button').previousElementSibling.firstChild.value);
+                value = Number.parseInt($(e.target.closest('button')).siblings('label').children('input').attr('value'));
                 if (value < 10) {
-                    e.target.closest('button').previousElementSibling.firstChild.value = value + 1;
+                    $(e.target.closest('button')).siblings('label').children('input').attr('value', value +1);
                 }
+
             }
         }
     });
