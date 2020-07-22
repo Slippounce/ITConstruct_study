0cb55b06453146d0f6c8390fddb553f3d177f339
Slippounce <sir.shmidt-crdoc@yandex.ru>
2020-07-22 15:39:22 +0700

    added data-type attr to control buttons

--- Блок2/template/assets/js/script.js
+++ Блок2/template/assets/js/script.js
@@ -3,13 +3,13 @@ window.onload = () => {
     document.querySelector('.cars').addEventListener('click', e =>{
         let value;
         if(e.target.closest('button')) {
-            if (e.target.closest('button').classList.contains('purchase-controls__minus-button')) {
+            if (e.target.closest('button').getAttribute('data-type') === 'minus') {
                 value = Number.parseInt(e.target.closest('button').nextElementSibling.firstChild.value);
                 if (value > 1) {
                     e.target.closest('button').nextElementSibling.firstChild.value = value - 1;
                 }
             }
-            if (e.target.closest('button').classList.contains('purchase-controls__plus-button')) {
+            if (e.target.closest('button').getAttribute('data-type') === 'plus') {
                 value = Number.parseInt(e.target.closest('button').previousElementSibling.firstChild.value);
                 if (value < 10) {
                     e.target.closest('button').previousElementSibling.firstChild.value = value + 1;
